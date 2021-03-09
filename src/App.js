707fe81c0314a4cb23e63e37181544ee5e88416f
marcoweb <marcocarvalho.web@gmail.com>
2021-03-09 23:14:37 +0000

    Init Todo List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Todo({ todo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Meu Aplicativo React No Firebase</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
+
+function App() {
+    const [todos, setTodos] = React.useState([
+        { text: "Fazer as Atividades da Quinzena" },
+        { text: "Estudar para PW3" },
+        { text: "Marcar Reunião para o TCC" },
+        { text: "Realizar os exercícios"}
+    ]);
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
